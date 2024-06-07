92c51d3f7a51253ad4cdaf505e20db3284df90d8
shanthosh04 <shanthosh1610@gmail.com>
2024-06-07 16:30:25 +0200

    add file

---
+++ scripts.js
@@ -0,0 +1,26 @@
+ /*Klasse Car repräsentiert ein Auto mit grundlegenden Eigenschaften.*/
+ class Car {
+    /*Konstruktor für die Car-Klasse.*/
+    constructor(brand, model, yearOfManufacture = null, seats = 5) {
+        this.brand = brand;
+        this.model = model;
+        this.yearOfManufacture = yearOfManufacture;
+        this.seats = seats;
+    }
+    /* Gibt eine Beschreibung des Autos zurück.*/
+    getDescription() {
+        return `${this.brand} ${this.model}, ${this.yearOfManufacture} ${this.seats}`;
+    }
+}
+/*Klasse ElectricCar erstellt und erbt von Car.*/
+class ElectricCar extends Car {
+    /*Konstruktor für die ElectricCar-Klasse.*/
+    constructor(brand, model, yearOfManufacture = null, seats = 5, batteryRange) {
+        super(brand, model, yearOfManufacture, seats);
+        this.batteryRange = batteryRange;
+    }
+    /*Gibt eine beschreibung des Elektroauto zurück.*/
+    getDescription() {
+        return `${this.brand} ${this.model}, ${this.yearOfManufacture} ${this.seats} ${this.batteryRange}`;
+    }
+}
