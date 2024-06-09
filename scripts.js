435d74d6d45ab2d3172581a99943b04c2d592e43
shanthosh04 <shanthosh1610@gmail.com>
2024-06-09 20:01:47 +0200

    fix error sperat html and js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
  /*Klasse Car repräsentiert ein Auto mit grundlegenden Eigenschaften.*/
- class Car {
+class Car {
     /*Konstruktor für die Car-Klasse.*/
     constructor(brand, model, yearOfManufacture = null, seats = 5) {
         this.brand = brand;
@@ -7,11 +7,13 @@
         this.yearOfManufacture = yearOfManufacture;
         this.seats = seats;
     }
+
     /* Gibt eine Beschreibung des Autos zurück.*/
     getDescription() {
         return `${this.brand} ${this.model}, ${this.yearOfManufacture} ${this.seats}`;
     }
 }
+
 /*Klasse ElectricCar erstellt und erbt von Car.*/
 class ElectricCar extends Car {
     /*Konstruktor für die ElectricCar-Klasse.*/
@@ -19,8 +21,16 @@ class ElectricCar extends Car {
         super(brand, model, yearOfManufacture, seats);
         this.batteryRange = batteryRange;
     }
+
     /*Gibt eine beschreibung des Elektroauto zurück.*/
     getDescription() {
         return `${this.brand} ${this.model}, ${this.yearOfManufacture} ${this.seats} ${this.batteryRange}`;
     }
 }
+
+
+const tesla = new ElectricCar('Tesla', 'Model 3', 2024, 5, 80)
+const description = tesla.getDescription()
+
+const h1 = document.getElementsByTagName('h1')[0] // array
+h1.innerText = description
